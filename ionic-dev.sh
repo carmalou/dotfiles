84950c0fa24c0de9e451170c47855cd011d98bc7
System Administrator <root@Carmens-MacBook-Pro.local>
2016-02-27 16:28:28 -0600

    adding some sh files

---
+++ ionic-dev.sh
@@ -0,0 +1,26 @@
+# stolen from Rick Yoesting https://github.com/ryoe/dotfiles/blob/master/ionic-dev.sh
+
+# Check for node
+if test ! $(which node); then
+  echo "You must install node..."
+fi
+
+if test $(which node); then
+  # gulp and bower because that's what you do!
+  sudo npm install -g gulp bower
+
+  # cordova and ionic are must haves
+  sudo npm install -g cordova ionic
+
+  # to deploy ios apps to simulator and devices
+  sudo npm install -g ios-sim ios-deploy
+
+  # karma is our test runner
+  sudo npm install -g karma-cli
+
+  # n - for managing node versions
+  sudo npm install -g n
+
+  # install node v0.12.8
+  # n 0.12.8
+fi
---
+++ node.sh
@@ -0,0 +1,5 @@
+# install node
+curl "https://nodejs.org/dist/latest/node-${VERSION:-$(wget -qO- https://nodejs.org/dist/latest/ | sed -nE 's|.*>node-(.*)\.pkg</a>.*|\1|p')}.pkg" > "$HOME/Downloads/node-latest.pkg" && sudo installer -store -pkg "$HOME/Downloads/node-latest.pkg" -target "/"
+
+# install http-server
+sudo npm install http-server -g
